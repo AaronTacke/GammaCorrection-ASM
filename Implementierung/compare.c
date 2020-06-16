a4cc726de2e018e22b195e0fc60a5814ebe6c677
Aaron Tacke <tackeaaron@gmail.com>
2020-06-16 12:01:08 +0200

    Added comments and improved the compare-method.

--- Implementierung/compare.c
+++ Implementierung/compare.c
@@ -2,30 +2,40 @@
 #include <stdint.h>
 #include <stdio.h>
 
-int calculate(int length, uint8_t *arr){
+//An implementation of the calculation to test and compare our assembly code to.
+void calculate(uint8_t *arr, int width, int height, float gamma){
+	//Values for a, b and c are going to be stored seperately later. 
+	//(And will be adapted again after implementation)
 	int a = 5;
-	int b = 7;
-	int c = 6;
-	float lambda = 0.5;
-	for(int i = 0; i < length; i+=3){
+	int b = 5;
+	int c = 5;
+	//We have 3 values for every pixel, so the total number of values is:
+	int length = width*height*3;
+	for(int i = 0; i < length; i+=3){ //For every pixel:
+		//Grayscale filter using formula (1) of the exercise
 		uint8_t d = (a * (*(arr+i)) + b * (*(arr+i+1)) + c * (*(arr+i+2))) / (a+b+c);
-		float gamma = powf((float)d / 255, lambda) * 255;
-		*(arr+i) = (uint8_t) gamma;
-		*(arr+i+1) = (uint8_t) gamma;
-		*(arr+i+2) = (uint8_t) gamma;
+		//Gamma control using formula (2) of the exercise
+		float p = powf((float)d / 255, gamma) * 255;
+		//Storing values back to array
+		*(arr+i) = (uint8_t) p;
+		*(arr+i+1) = (uint8_t) p;
+		*(arr+i+2) = (uint8_t) p;
 	}
-	return 0;
 }
 
+//A test implementation that prints the result of calculate
 int main(int argc, char** argv) {
-	int length = 300;
-	uint8_t x[length];
+	int length = 256;
+	float gamma = 1.5;
+	uint8_t x[3*length];
 	for(int i = 0; i < length; i++){
-		x[i] = ((i*7)%256);
+		x[3*i] = (i/3)*3;
+		x[3*i+1] = (i/3)*3;
+		x[3*i+2] = (i/3)*3;
 	}
-	printf("%i\n",calculate(length, x));
+	calculate(x, length, 1, gamma);
 	for(int i = 0; i < length; i++){
-		printf("%u\n", x[i]);
+		printf("%u, %u, %u\n", x[3*i],x[3*i+1],x[3*i+2]);
 	}
 	return 0;
 }
