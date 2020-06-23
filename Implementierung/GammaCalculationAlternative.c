2c12d8ee65f1e44046868d0de8bc4c9bc30adcd4
Philip Haitzer <philip@wundergraben.de>
2020-06-23 19:35:55 +0200

    Added new function to calculate the result of the gamma operation with binary search

--- Implementierung/GammaCalculationAlternative.c
+++ Implementierung/GammaCalculationAlternative.c
@@ -1,6 +1,8 @@
 #include <math.h>
 #include <stdint.h>
 #include <stdio.h>
+#include "GammaFunctions.h"
+#include "GammaValues.h"
 
 int counterSteps = 0;
 
@@ -43,8 +45,55 @@ float fastPower(float base, int exponent){
     return result;
 }
 
+int ggT(int a, int b){
+    if(b == 0)
+        return a;
+    else return ggT(b, a % b);
+}
+
+uint8_t calculateResult(float gamma, int input){
+    int n = 1;
+    int z = round(gamma);
+    float maxErr = 0.00005;
+    while (gamma - z > maxErr){
+        gamma *= 10;
+        z = round(gamma);
+        n *= 10;
+    }
+    //used to create smaller values for z and n
+    int ggt = ggT(z, n);
+    z /= ggt;
+    n /= ggt;
+    //printf("z: %d n: %d\n", z, n);
+    float a = fastPower((float)input / 255, z);
+
+    uint8_t min = 0;
+    uint8_t max = 255;
+    int middle;
+    while(min < max){
+        middle = (min+max)/2+1;
+        //printf("middle: %d\n", middle);
+        if(fastPower((float)middle / 255, n) > a){
+            max = middle - 1;
+        }else{
+            min = middle;
+        }
+    }
+    //printf("result: %d", min);
+    return min;
+}
+
 int main() {
-    float result = fastPower(5.19, 9);
-    printf("%f\n", result);
-    printf("%d", counterSteps);
+    int counter = 0;
+    for (int i = 0; i < sizeof(gammaValues) / sizeof(uint8_t); ++i) {
+        for (int j = 0; j <= 255; ++j) {
+            uint8_t ourResult = calculateResult(gammaValues[i], j);
+            if (gammaFunctions[counter] != ourResult) {
+                printf("wrong result, our Result: %d, actual result: %d counter %d\n", ourResult,
+                       gammaFunctions[counter], counter);
+            }
+            counter++;
+        }
+
+    }
 }
