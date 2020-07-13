910f25f26fae627bd227cc3f6674e8729f929680
Philip Haitzer <email>
2020-07-13 21:28:29 +0200

    Fixed errors in calculateWithHardcodedFunctions.c and decodeFunction.S

--- Implementierung/(OUTDATED)hardcoded_functions/calculateWithHardcodedFunctions.c
+++ Implementierung/(OUTDATED)hardcoded_functions/calculateWithHardcodedFunctions.c
@@ -41,13 +41,12 @@ void calculateOptimized(uint8_t *arr, int width, int height, float gamma){
     }
     */
     void decode(uint8_t *result, int indexOfFunction){
-        decodeFunction(compressedFunctionsSmall[indexOfFunction * 8], result)
+        decodeFunction(compressedFunctionsSmall[indexOfFunction * 8], result);
     }
     void main() {
         uint8_t *result[256];
         decode(result, 1);
         for (int i = 0; i < 256; ++i) {
-            printf("%d\n", result[i])
+            printf("%d\n", result[i]);
         }
     }
-}
